1475f01be50488d74b53bcdec415e2a6a9951e19
kbus9455 <katarzyna.bus9455@gmail.com>
2021-10-29 22:27:36 +0200

    add cart display (empty)

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -1,6 +1,10 @@
+
+// Show and hide menu 
+
 const menuButton = document.querySelector('div.menu');
 const menuClose = document.querySelector('li.close');
-const menuMobile = document.querySelector('.menu-mobile')
+const menuMobile = document.querySelector('.menu-mobile');
+
 menuButton.addEventListener('click', function(){
 menuMobile.classList.toggle('active');
 console.log('dziala')
@@ -8,5 +12,15 @@ console.log('dziala')
 
 menuClose.addEventListener('click', function(){
 menuMobile.classList.toggle('active');
+console.log('dziala')
+});
+
+// Display cart 
+
+const cart = document.querySelector('div.cart');
+const displayCart = document.querySelector('div.displayCart');
+
+cart.addEventListener('click', function(){
+displayCart.classList.toggle('active');
 console.log('dziala')
 });
